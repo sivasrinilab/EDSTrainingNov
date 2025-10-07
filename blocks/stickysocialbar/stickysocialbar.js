e6598df553b34ac21a26470aa882a461f0b252c6
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2025-10-07 23:16:30 +0530

    Update stickysocialbar.js

--- blocks/stickysocialbar/stickysocialbar.js
+++ blocks/stickysocialbar/stickysocialbar.js
@@ -3,7 +3,7 @@ const elem = document.querySelectorAll('.stickysocialbar ul li');
 
 
  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < elem.length; i++) {
+  for (var i = 0; i < elem.length; i++) {
     a = elem[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
      // Create a new input element

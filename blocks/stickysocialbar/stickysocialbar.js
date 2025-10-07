3cc196a9f59cf3dfdcaec3c9564909f7a87e33bd
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2025-10-07 23:02:22 +0530

    Update stickysocialbar.js

--- blocks/stickysocialbar/stickysocialbar.js
+++ blocks/stickysocialbar/stickysocialbar.js
@@ -1,4 +1,13 @@
 const elem = document.querySelector('.stickysocialbar');
 const ulelem = elem.closest('ul')
-   li = ulelem.getElementsByTagName('li');
-<i class="fa fa-facebook"></i>
+li = ulelem.getElementsByTagName('li');
+ // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+     // Create a new input element
+  const inputElement = document.createElement('i');
+    inputElement.setAttribute('class', 'fa fa-'+txtValue);
+ a.append(inputElement);
+  }
+}
